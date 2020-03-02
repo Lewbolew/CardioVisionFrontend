41736d6b8cf0e5e09d0cdace9784f67a64c2868a
Andriy <borovec@ucu.edu.ua>
2020-03-02 15:50:39 +0200

    Updates

---
+++ precache-manifest.addecd11e97f797337092db283bcd866.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "824385b63d5a3a486fad7e313f268def",
+    "url": "/CardioVisionFrontend/index.html"
+  },
+  {
+    "revision": "d819f6b11b5c1f0bea27",
+    "url": "/CardioVisionFrontend/static/css/main.76e27cc9.chunk.css"
+  },
+  {
+    "revision": "93b826958bb404c37297",
+    "url": "/CardioVisionFrontend/static/js/2.4bf962a1.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/CardioVisionFrontend/static/js/2.4bf962a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d819f6b11b5c1f0bea27",
+    "url": "/CardioVisionFrontend/static/js/main.2a998f56.chunk.js"
+  },
+  {
+    "revision": "1f039bf29b709e2d2b60",
+    "url": "/CardioVisionFrontend/static/js/runtime-main.c19e92df.js"
+  },
+  {
+    "revision": "8915baec0c39ca64281615432cf40cee",
+    "url": "/CardioVisionFrontend/static/media/logo-full.8915baec.svg"
+  },
+  {
+    "revision": "2b76f8db787743bdfb1bfa4d6cefa879",
+    "url": "/CardioVisionFrontend/static/media/logo-short.2b76f8db.svg"
+  }
+]);
